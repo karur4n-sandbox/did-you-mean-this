7e03d59c73f577af67b31a00857036f3a3f3d2c1
古河 和樹 <k.furukawa@snowrobin.jp>
2019-08-27 15:46:04 +0900

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Did you mean this?
+
+[hiddentao/fast\-levenshtein](https://github.com/hiddentao/fast-levenshtein) を使いたかっただけ
+
+```sh
+$ npm run start build:dev
+$ npm run start build:de
+$ npm run start build:prodccc
+```
---
+++ index.ts
@@ -0,0 +1,65 @@
+const exec = require("child_process").exec;
+const levenshtein = require("fast-levenshtein");
+
+type Command = {
+  [key: string]: string;
+};
+
+const commands: Command = {
+  "build:dev": "echo dev",
+  "build:production": "echo production"
+};
+
+const commandKey = process.argv[2];
+
+if (commandKey == undefined) {
+  process.stdout.write("コマンドを与えてください\n");
+  process.exit(1);
+}
+
+const commandValue = commands[commandKey];
+
+if (commandValue) {
+  exec(commandValue, (_: Error, stdout: string) => {
+    process.stdout.write(stdout);
+    process.exit(0);
+  });
+} else {
+  // [コマンド, レーベンシュタイン距離]
+  type LevenshteinCount = [string, number];
+
+  const levenshteinCounts = Object.keys(commands).map(
+    (key): LevenshteinCount => [key, levenshtein.get(key, commandKey)]
+  );
+
+  const minimumLevenshtein = minimum(levenshteinCounts, (a, b) => a[1] - b[1]);
+
+  if (minimumLevenshtein[1] < 6) {
+    process.stdout.write("Did you mean this?\n");
+    process.stdout.write(minimumLevenshtein[0] + "\n");
+  }
+}
+
+//
+// minimum
+//
+function minimum<T>(
+  arry: T[],
+  iteratee: MinimumCompare<T> | undefined
+): T | undefined {
+  if (arry.length === 1) {
+    return undefined;
+  }
+
+  const sorted = (() => {
+    if (typeof iteratee == "function") {
+      return [...arry].sort(iteratee);
+    } else {
+      return [...arry].sort();
+    }
+  })();
+
+  return sorted[0];
+}
+
+type MinimumCompare<T> = (a: T, b: T) => number;
